d1d1c4a28473e0d4fb86e04c3d5f3a8ecc3d690a
RongY <yang.ariel@hotmail.com>
2020-11-04 17:27:41 -0500

    update

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fetchBreedDescription = (searchStr, callback ) => {
   let url = `https://api.thecatapi.com/v1/breeds/search?q=${searchStr}`;
   request.get(url, (error, response, body) => {
-    if ( !error) {
+    if (!error && body) {
       const data = JSON.parse(body);
       callback(null, data[0]['description']);
     } else {
